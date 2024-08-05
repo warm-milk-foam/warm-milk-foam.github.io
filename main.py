d0895743af793b000a0acd15eb7cbcc240688879
chocolate_milk <129393705+warm-milk-foam@users.noreply.github.com>
2024-08-05 17:22:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from flask import Flask
 from flask import Flask, render_template
 import requests
-app = Flask(__name__, static_folder='C:/Users/student/Downloads/hello_flask/static', template_folder='C:/Users/student/Downloads/hello_flask/templates')
+
+app = Flask(__name__, static_folder="C:/Users/XXXXXXXXXX/OneDrive - Anglo-Chinese School (Independent)/flask_website_ingo", template_folder='C:/Users/XXXXXXXXXX/OneDrive - Anglo-Chinese School (Independent)/flask_website_ingo')
+
 
 @app.route("/")
 def home():
-  return render_template('index.html')
-  
+  return render_template('home.html')
+
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run()
